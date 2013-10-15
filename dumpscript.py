55ee7bfc9df19595722234847721cd88c2685649
Anonymous <none>
2013-10-15 04:13:40 -0700

    script dumper gets full message list

--- dumpscript.py
+++ dumpscript.py
@@ -18,20 +18,21 @@ if __name__ == "__main__":
 
     infile = open(infile, "rb")
     outfile = open(outfile, "w+")
-    address = int(address, 16) - 0x930
+    address = int(address, 16)
     while True:
         if address >= END_ADDRESS:
             break
 
-        print "%x" % address,
+        print "%x" % address
         try:
             d = Datapack(infile=infile, address=address)
-            messages = d.extract_messages()
-            messages = map(gen_formatted, messages)
-            outfile.write("#-------\n#{0:0>7}\n#-------\n\n".format("%x" % address))
-            for num, message in enumerate(messages):
-                outfile.write("#<{0:0>2}.>\n{1}\n\n".format(num + 1, message))
-            print
+            for i, messages in enumerate(d.messageslist):
+                if messages is None:
+                    continue
+                messages = map(gen_formatted, messages)
+                outfile.write("#---------\n#{0:0>7}.{1}\n#---------\n\n".format("%x" % address, i))
+                for num, message in enumerate(messages):
+                    outfile.write("#<{0:0>2}.>\n{1}\n\n".format(num + 1, message))
         except (AssertionError, IndexError), e:
             # TODO: need to double check each assertion, probably too lenient
             # for example, still won't dump message at 1165fa
