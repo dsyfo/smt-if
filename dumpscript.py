0e3450c4f69c7c0ea59339473a8316d63ba4fc4d
Anonymous <none>
2013-10-15 21:08:10 -0700

    cleaner format for script dumps

--- dumpscript.py
+++ dumpscript.py
@@ -1,8 +1,14 @@
+# -*- coding: utf-8 -*-
 from sys import argv
 from superdecompressor import Datapack
 from utils import gen_formatted
 
 END_ADDRESS = 0xf489540
+specials = {}
+for line in open("specialcodes.txt"):
+    code, replacement = tuple(line.strip().split(' ', 1))
+    replacement = replacement.replace(r"\n", "\n")
+    specials[code] = replacement
 
 if __name__ == "__main__":
     if len(argv) < 3:
@@ -30,9 +36,12 @@ if __name__ == "__main__":
                 if messages is None:
                     continue
                 messages = map(gen_formatted, messages)
-                outfile.write("#---------\n#{0:0>7}.{1}\n#---------\n\n".format("%x" % address, i))
+                outfile.write("#<@{0:0>7}>\n".format("%x" % address))
                 for num, message in enumerate(messages):
-                    outfile.write("#<{0:0>2}.>\n{1}\n\n".format(num + 1, message))
+                    output = "#<{2}.{0:0>2}>\n{1}\n\n".format(num + 1, message, i)
+                    for code, replacement in specials.items():
+                        output = output.replace(code, replacement)
+                    outfile.write(output)
         except (AssertionError, IndexError), e:
             # TODO: need to double check each assertion, probably too lenient
             # for example, still won't dump message at 1165fa
